ecd7559845c2f9ff6b8f2df5408aa89b0f82bef4
Corey Sessions <corey.iosdev@yahoo.com>
2019-05-01 22:01:00 -0600

    Created Question.Swift File

---
+++ Q&A Project/Q&A Project/Question.swift
@@ -0,0 +1,25 @@
+//
+//  Question.swift
+//  Q&A Project
+//
+//  Created by Corey Sessions on 5/1/19.
+//  Copyright © 2019 Corey Sessions. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var question:   String
+    var asker:      String
+    var answer:     String?
+    var answerer:   String?
+    
+    
+    init(question: String, asker: String, answer: String? = nil, answerer: String? = nil) {
+        self.question   = question
+        self.asker      = asker
+        self.answer     = answer
+        self.answerer   = answerer
+    }
+    
+}
---
+++ Q&A Project/Q&A Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Q&A Project
+//
+//  Created by Corey Sessions on 5/1/19.
+//  Copyright © 2019 Corey Sessions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
