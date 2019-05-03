01bb9e7b8285418552cf477f1a3015ce8439e566
Corey Sessions <corey.iosdev@yahoo.com>
2019-05-02 23:25:51 -0600

    Setup TableView UI and added Outlets

---
+++ Q&A Project/Q&A Project/QuestionTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionTableViewCell.swift
+//  Q&A Project
+//
+//  Created by Corey Sessions on 5/2/19.
+//  Copyright © 2019 Corey Sessions. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var questionLbl: UILabel!
+    @IBOutlet weak var askLbl: UILabel!
+    
+
+}
